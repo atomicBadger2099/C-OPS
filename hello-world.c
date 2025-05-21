b959852b401b96727a9e553f604d295a94a948fd
dwayne brock <119648758+atomicBadger2099@users.noreply.github.com>
2025-05-21 16:12:51 -0500

    Add files via upload

---
+++ hello-world.c
@@ -0,0 +1,15 @@
+//HELLO WORLD IN C (YES THIS IS A COMMENT!)
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("\n");
+    printf("------------------------------------------------------------\n");
+    printf("Hello, World!\n");
+    printf("------------------------------------------------------------\n");
+
+    printf("\n");
+
+    return 0;
+}
